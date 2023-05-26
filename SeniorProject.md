347983ee8cb630b950822ced7e15add3b625c685
rcalub <regino.john.calub@gmail.com>
2023-05-25 23:12:38 -0700

    Created senior project mark down file

---
+++ SeniorProject.md
@@ -0,0 +1,6 @@
+# Chapter 0: Introduction
+## Why Rubik's Cubes
+When most people think of the applications of mathematics, often times they'll picture engineering, chemistry, physics, or other simillar fields in STEM. However, I feel that the most application of mathematics is the 
+
+# Chapter 1: Group Theory
+## 1.1: Groups
--- src/App.js
+++ src/App.js
@@ -44,16 +44,11 @@ function App() {
                 <div class="box-main">
                     <div class="secondHalf">
                         <h1 class="text-big" id="program">
-                            Group Theory
+                            Chapter 1: Group Theory
                         </h1>
                         <p class="text-small">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
-                            enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                            culpa qui officia deserunt mollit anim id est laborum.
+                            <h2> Section 1.1: Groups </h2>
+                            <p></p>
                         </p>
                     </div>
                 </div>
